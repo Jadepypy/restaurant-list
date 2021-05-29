6ca18184918ae06e4b9bd0d074af8afb5218c342
Jadepypy <questiongirl829@gmail.com>
2021-05-29 23:22:42 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -46,19 +46,58 @@ app.get('/search', (req, res) =>{
   const searchResult = restaurants.results.filter((item) => item.name.toLowerCase().includes(keyword.toLowerCase()))
   res.render('index', {restaurants: searchResult, keyword: keyword})
 })
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', {restaurant}))
+    .catch( error => console.log(error))
+})
 app.post('/restaurants', (req, res) => {
   const name = req.body.name
   const name_en = req.body.name_en
   const category = req.body.category
-  const address = req.body.address
+  const location = req.body.location
   const phone = req.body.phone
   const image = req.body.image
   const rating = req.body.rating
   const description = req.body.description
-  return Restaurant.create({category, address, phone, image, rating,description})
+  return Restaurant.create({name, name_en, category, location, phone, image, rating,description})
             .then(() => res.redirect('/'))
             .catch(error => console.log(error))
 
+})
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const location = req.body.location
+  const phone = req.body.phone
+  const image = req.body.image
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.image = image
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () =>{
