bccd19ea21e52b699fe4da0890259114751a1551
Jadepypy <questiongirl829@gmail.com>
2021-05-29 21:47:17 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Restaurant = require('./models/restaurant')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true})
@@ -22,11 +23,16 @@ app.set('view engine', 'handlebars')
 // setting static files, generating routes
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) =>{
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', {restaurants: restaurants}))
     .catch(error => console.log(error))
+})
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
 })
 app.get('/restaurants/:id', (req, res) =>{
   const id = req.params.id
@@ -39,6 +45,20 @@ app.get('/search', (req, res) =>{
   const keyword = req.query.keyword
   const searchResult = restaurants.results.filter((item) => item.name.toLowerCase().includes(keyword.toLowerCase()))
   res.render('index', {restaurants: searchResult, keyword: keyword})
+})
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const address = req.body.address
+  const phone = req.body.phone
+  const image = req.body.image
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.create({category, address, phone, image, rating,description})
+            .then(() => res.redirect('/'))
+            .catch(error => console.log(error))
+
 })
 
 app.listen(port, () =>{
