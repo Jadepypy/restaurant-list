cc9076ee9c1fe527256cae9a87a5fca18bcc4e37
Jadepypy <questiongirl829@gmail.com>
2021-06-03 23:45:17 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const Restaurant = require('./models/restaurant')
+const routes = require('./routes')
+
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true})
@@ -25,80 +27,12 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/', (req, res) =>{
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', {restaurants: restaurants}))
-    .catch(error => console.log(error))
-})
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-app.get('/restaurants/:id', (req, res) =>{
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', {restaurant}))
-    .catch((error) => console.log(error))
-})
 app.get('/search', (req, res) =>{
   const keyword = req.query.keyword
   const searchResult = restaurants.results.filter((item) => item.name.toLowerCase().includes(keyword.toLowerCase()))
   res.render('index', {restaurants: searchResult, keyword: keyword})
-})
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('edit', {restaurant}))
-    .catch( error => console.log(error))
-})
-app.post('/restaurants', (req, res) => {
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const location = req.body.location
-  const phone = req.body.phone
-  const image = req.body.image
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.create({name, name_en, category, location, phone, image, rating,description})
-            .then(() => res.redirect('/'))
-            .catch(error => console.log(error))
-
-})
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const location = req.body.location
-  const phone = req.body.phone
-  const image = req.body.image
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.image = image
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
 })
 
 app.listen(port, () =>{
